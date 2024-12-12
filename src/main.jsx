d0b37e44c3dbbb42d03fe439a73f2602a4d19c3c
benjamin almiron <almironbenjamin2001@gmail.com>
2024-12-12 13:52:27 -0300

    -

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootDOMnode = document.getElementById("root");
+const reactRoot = createRoot(rootDOMnode);
 
-
-/* const rootDOMnode = document.getElementById("root") 
-const reactRoot = createRoot(rootDOMnode)
-
-reactRoot.render(<StrictMode><App></App></StrictMode>) */
+reactRoot.render(<App />);
